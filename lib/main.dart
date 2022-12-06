7606af8ac5a9054d774a16c9a1cf7cbe5657ca2f
Lilou <tesinetl@gmail.com>
2022-12-06 13:42:28 +0100

    add burger menu

--- lib/main.dart
+++ lib/main.dart
@@ -13,139 +13,158 @@ Future<void> main() async {
     MaterialApp(
       title: 'Plants', // used by the OS task switcher
       home: SafeArea(
-        child: MyScaffold(
-          plantlist: fields,
-        ),
+        child: MyScaffold(plantlist: fields,),
       ),
     ),
   );
 }
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
-  // Fields in a Widget subclass are always marked "final".
-  final Widget title;
+//------------------------Plants page------------------------
+  class MyScaffold extends StatelessWidget {
+    const MyScaffold({required this.plantlist, super.key});
+    final List<List<dynamic>> plantlist;
+    @override
+    Widget build(BuildContext context) {
+      // Material is a conceptual piece of paper on which the UI appears.
+      return Scaffold(
+        appBar: AppBar(title: const Text('Plantes'),),
+        body : Container(
+            child: MyList(plantlist: plantlist),
+        ),
+        drawer: const MyDrawer(),
+      );
+    }
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(
-                Icons.local_florist,
-                color: Colors.white ,
+  class MyList extends StatelessWidget {
+    const MyList({required this.plantlist, super.key});
+    final List<List<dynamic>> plantlist;
+    
+    @override
+    Widget build(BuildContext context) {
+      return ListView.builder(
+          itemCount: plantlist.length,
+          itemBuilder: (context, index) {
+            String trailingText ;
+            if (plantlist[index][20] == '' ) {
+              trailingText = '';
+            } else {
+              trailingText = '${plantlist[index][20]} m';
+              }
+            return ListTile(
+              title: Text(plantlist[index][0]),
+              subtitle: Text(plantlist[index][1]),
+              trailing:  Text(trailingText),
+            );
+          },
+      );
+    }
+  }
+
+  class MyDrawer extends StatelessWidget {
+    const MyDrawer({super.key});
+    @override
+    Widget build(BuildContext context) {
+      return Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
                 ),
-            tooltip: 'plantes',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyBody extends StatelessWidget {
-  const MyBody({required this.plantlist, super.key});
-  final List<List<dynamic>> plantlist;
-  
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child : ListView.builder(
-        itemCount: plantlist.length,
-        itemBuilder: (context, index) {
-          String trailingText ;
-          if (plantlist[index][20] == '' ) {
-            trailingText = '';
-          } else {
-            trailingText = '${plantlist[index][20]} m';
-            }
-          return ListTile(
-            title: Text(plantlist[index][0]),
-            subtitle: Text(plantlist[index][1]),
-            trailing:  Text(trailingText),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MyTable extends StatelessWidget {
-  const MyTable({required this.plantlist, super.key});
-  final List<List<dynamic>> plantlist;
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      //width: double.infinity,
-      child: DataTable(
-        columns: const <DataColumn>[
-          DataColumn(
-            label: Text('Nom'),
-          ),
-          DataColumn(
-            label: Text('Nom latin'),
-          ),
-        ],
-        rows: List<DataRow>.generate(
-          plantlist.length,
-          (int index) => DataRow(
-            cells: <DataCell>[
-              DataCell(
-                Text(plantlist[index][0])
+                child: Text('Menu'),
               ),
-              DataCell(
-                Text(plantlist[index][2])
-              )
+              ListTile(
+                title: const Text('Plantes'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Zones'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
             ],
           ),
-        ),
-      ),
-    );
+      );
+    }
   }
-}
 
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({required this.plantlist, super.key});
-  final List<List<dynamic>> plantlist;
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Plantes',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .headline6,
+  /* class MyTable extends StatelessWidget {
+    const MyTable({required this.plantlist, super.key});
+    final List<List<dynamic>> plantlist;
+    @override
+    Widget build(BuildContext context) {
+      return DataTable(
+          columns: const <DataColumn>[
+            DataColumn(label: Text('Nom'),),
+            DataColumn(label: Text('Nom latin'),),
+          ],
+          rows: List<DataRow>.generate(
+            plantlist.length,
+            (int index) => DataRow(
+              cells: <DataCell>[
+                DataCell(Text(plantlist[index][0])),
+                DataCell(Text(plantlist[index][2]))
+              ],
             ),
           ),
-
-          MyBody(
-            plantlist: plantlist
-          )
-
-        ]
-      ),
-    );
+      );
+    }
   }
-}
+  */
 
+  /* class MyAppBar extends StatelessWidget {
+    const MyAppBar({required this.title, super.key});
+    // Fields in a Widget subclass are always marked "final".
+    final Widget title;
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        height: 56.0, // in logical pixels
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(color: Colors.blue[500]),
+        // Row is a horizontal, linear layout.
+        child: Row(
+          children: [
+            const IconButton(
+              icon: Icon(
+                  Icons.local_florist,
+                  color: Colors.white ,
+                  ),
+              tooltip: 'plantes',
+              onPressed: null, // null disables the button
+            ),
+            // Expanded expands its child
+            // to fill the available space.
+            Expanded(
+              child: title,
+            ),
+            const IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  */
+
+//------------------------Zones Page------------------------
