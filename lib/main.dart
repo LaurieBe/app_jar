7ff79b6a9ef86c7c8b2aac3b299bf16301341a43
Lilou <tesinetl@gmail.com>
2022-12-16 18:32:12 +0100

    open plant page

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,34 @@ import 'package:app_jar/plant.dart';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 
+
 Future<void> main() async {
   //récupérer la liste de plantes à partir du fichier CSV
-  final input =
-      File('C:\\LBE_Flutter\\app_JAR\\app_jar\\assets\\caracteristiques.csv')
-          .openRead();
-  List<List<dynamic>> fieldsDyn = await input
-      .transform(utf8.decoder)
-      .transform(const CsvToListConverter(fieldDelimiter: ';'))
-      .toList();
+  final input = File('C:\\LBE_Flutter\\app_JAR\\app_jar\\assets\\caracteristiques.csv').openRead();
+  List<List<dynamic>> fieldsDyn = await input.transform(utf8.decoder).transform(const CsvToListConverter(fieldDelimiter: ';')).toList();
 
+  //définir les colonnes correspondant à chacun des champs de plant et en faire une liste
   List<Plant> plantList = [];
   for (List<dynamic> line in fieldsDyn) {
     num? size = line[20] is num ? line[20] : null;
+    String? hardiness;
+    if (line[7] is num) {hardiness = line[7].toString();} 
+    else {hardiness = line[7];}
+
     plantList.add(
-        Plant(name: line[0], size: size?.toDouble(), scientificName: line[1]));
+      Plant(
+        name: line[0], 
+        size: size?.toDouble(), 
+        scientificName: line[1],
+        exposure: line[3],
+        soil: line[4],
+        ph: line[5],
+        watering: line[6],
+        hardiness: hardiness,
+        type: line[8],
+        area: line[38],
+      )
+    );
   }
 
   runApp(
@@ -75,16 +88,9 @@ class MyHomePage extends StatelessWidget {
               child: ElevatedButton.icon(
                 icon: const Icon(Icons.map),
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MyAreaPage(
-                                plantList: plantList,
-                              )));
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => MyAreaPage(plantList: plantList,)));
                 },
-                label: const Text(
-                  'ZONES',
-                  textScaleFactor: 1.5,
+                label: const Text('ZONES',textScaleFactor: 1.5,
                 ),
               ),
             ),
--- lib/plant.dart
+++ lib/plant.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Plant {
-  const Plant({required this.name, this.scientificName, this.type, this.size});
+  const Plant({required this.name, this.scientificName, this.type, this.size, this.exposure,this.hardiness,this.ph,this.soil,this.watering,this.area});
   final String name;
   final String? scientificName;
   final String? type;
   final double? size;
+  final String? exposure;
+  final String? soil;
+  final String? ph;
+  final String? watering;
+  final String? hardiness;
+  final String? area;
+
 }
 
-//------------------------Plants page------------------------
+//------------------------PlantList page------------------------
 
 class MyPlantListPage extends StatelessWidget {
   const MyPlantListPage({required this.plantList, super.key});
@@ -17,17 +24,13 @@ class MyPlantListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Material is a conceptual piece of paper on which the UI appears.
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plantes'),
-      ),
-      body: Container(
-        child: MyPlantList(plantList: plantList),
-      ),
+      appBar: AppBar(title: const Text('Plantes'),),
+      body: Container(child: MyPlantList(plantList: plantList),),
       //drawer: MyDrawer(plantlist: plantlist),
-      floatingActionButton: FloatingActionButton(
+/*    floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
-      ),
+      ), */
     );
   }
 }
@@ -52,12 +55,8 @@ class MyPlantList extends StatelessWidget {
           subtitle: Text(plantList[index].scientificName ?? ''),
           trailing: Text(trailingText),
           onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => MyPlantPage(
-                          plantList: plantList,
-                        )));
+            Navigator.push(context,MaterialPageRoute(
+              builder: (context) => MyPlantPage(plantList: plantList, index:index)));
           },
           //...plantlist.map((e) => MyPlantTile(plant:e)),
         );
@@ -66,23 +65,30 @@ class MyPlantList extends StatelessWidget {
   }
 }
 
+//------------------------Plant page------------------------
+
 class MyPlantPage extends StatelessWidget {
-  const MyPlantPage({required this.plantList, super.key});
+  const MyPlantPage({required this.plantList, required this.index, super.key});
   final List<Plant> plantList;
+  final int index;
+  
   @override
   Widget build(BuildContext context) {
     // Material is a conceptual piece of paper on which the UI appears.
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Plantes'),
+        title: Text(plantList[index].name),
       ),
-      body: Container(
-        child: MyPlantList(plantList: plantList),
-      ),
-      //drawer: MyDrawer(plantlist: plantlist),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      body: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(plantList[index].scientificName ?? ''),
+              subtitle: Text(plantList[index].type ?? ''),
+              trailing: Text(plantList[index].hardiness ?? ''),
+            ),
+          ],
+        ),
       ),
     );
   }
